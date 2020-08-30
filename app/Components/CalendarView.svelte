d47fe4dc603ab451c991e409f4cb52b376c423a6
Hussariush <aethelbrihte@gmail.com>
2020-08-30 16:29:29 +0200

    Items layout and initial calculations

--- app/Components/CalendarView.svelte
+++ app/Components/CalendarView.svelte
@@ -1,40 +1,37 @@
-<page>
-    <actionBar title="I Allow It" />
-    <stackLayout>
-            <!-- <listView height='150' items="{numberOfSubtractInputs}" on:itemTap={onItemTap} row='1'> 
-                <Template let:item> 
-                <label>
-                    <span text={item} />
-                </label>
-                <button text="rem" on:tap="{onRemove(item)}" />
-                </Template>
-            </listView>
-            <textField required editable="true" keyboardType={'number'} bind:text="{tempDeduction}" on:textChange={onAddDeduction} hint={'Subtract this'}/>
-            <button text="Deduct" isEnabled={!!tempDeduction} on:tap="{onDeduct}" />
-        </stackLayout> -->
-        <button text="Back" on:tap="{goBack}" />
-        <button text="is cal view {daysInMonth} {$totalAmount}" />
-</page>
-
 <script>
-import { Template } from 'svelte-native/components'
-import { totalAmount } from '../Stores/stores.js'
-// import debounce from 'lodash/debounce'
-import { closeModal, goBack } from 'svelte-native';
-const getDaysInMonth = () => {
-   const m = new Date().getMonth();
+  import { Template } from "svelte-native/components";
+  import { totalAmount } from "../Stores/stores.js";
+  // import debounce from 'lodash/debounce'
+  import { closeModal, goBack } from "svelte-native";
+  const getDaysInMonth = () => {
+    const m = new Date().getMonth();
     const y = new Date().getFullYear();
-    return new Date(y,m,0).getDate();
+    return new Date(y, m, 0).getDate();
   };
 
-    let daysInMonth = getDaysInMonth();
-//     let numberOfSubtractInputs = [] 
-//     let salary = 0;
-//     let tempDeduction = null;
-
-        
- </script>
+  let daysInMonth = getDaysInMonth();
+  let allowanceDays = [...new Array(daysInMonth)].map(()=> +$totalAmount / +daysInMonth);
+  console.log(allowanceDays);
+</script>
 
 <style>
-   
+
 </style>
+
+<page>
+  <actionBar title="I Allow It" />
+  <dockLayout backgroundColor="yellow">
+    <scrollView dock='top' backgroundColor="goldenrod">
+      <listView height='500' items={allowanceDays} backgroundColor="blue">
+        <Template let:item >
+          <label
+          width="60"
+          backgroundColor="limegreen">
+          <span text={parseFloat(item).toFixed(2)} />
+        </label>
+      </Template>
+    </listView>
+  </scrollView>
+  <button dock='bottom' text="Back" on:tap={goBack} />
+</dockLayout>
+</page>
