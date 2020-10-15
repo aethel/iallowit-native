dfe7848703a535711535d985786b2fdf5c77232a
Hussariush <aethelbrihte@gmail.com>
2020-10-15 21:41:16 +0200

    Resuming existing breakdown

--- app/Components/CalendarView.svelte
+++ app/Components/CalendarView.svelte
@@ -2,14 +2,14 @@
   import { Template } from "svelte-native/components";
   import { totalAmount, monthlyBreakdown } from "../Stores/stores.js";
   // import debounce from 'lodash/debounce'
-  import EditDay from './EditDay';
-  import localStorage from 'nativescript-localstorage';
+  import EditDay from "./EditDay";
+  import localStorage from "nativescript-localstorage";
 
   import { showModal, closeModal, goBack } from "svelte-native";
   const getDaysInMonth = () => {
     const m = new Date().getMonth();
     const y = new Date().getFullYear();
-    return new Date(y, m+1, 0).getDate();
+    return new Date(y, m + 1, 0).getDate();
   };
   const dayInMonth = new Date().getDate();
   const allowanceObject = {
@@ -19,53 +19,63 @@
   };
 
   let daysInMonth = getDaysInMonth();
-  const localBreakdown = localStorage.getItem('breakdown');
+  const localBreakdown = localStorage.getItem("breakdown");
   let allowanceDays = [...new Array(daysInMonth)].map(() => {
-    return { allowance: parseInt($totalAmount,10) /parseInt(daysInMonth,10), deductions: [], additions:[] };
+    return {
+      allowance: parseInt($totalAmount, 10) / parseInt(daysInMonth, 10),
+      deductions: [],
+      additions: [],
+    };
   });
-  localBreakdown ? monthlyBreakdown.set(JSON.parse(localBreakdown)):  monthlyBreakdown.set(allowanceDays)
-  console.log(JSON.parse(localBreakdown));
+  localBreakdown
+    ? monthlyBreakdown.set(JSON.parse(localBreakdown))
+    : monthlyBreakdown.set(allowanceDays);
   $: allowances = $monthlyBreakdown;
   let totalDeduction = null;
 
   const updateAllowancesFromTodays = (arr) => {
     const daysFromToday = arr.splice(dayInMonth);
-    const daysUntilToday = arr.splice(0,dayInMonth);
-    const sumOfPreviousAllowances = daysUntilToday.map(item => {
-            return item.allowance || 0;
-    }).reduce((acc,curr) => acc + curr);
-    const sumOfFutureAllowances = daysFromToday.map(item => {
+    const daysUntilToday = arr.splice(0, dayInMonth);
+    const sumOfPreviousAllowances = daysUntilToday
+      .map((item) => {
         return item.allowance || 0;
-}).reduce((acc,curr) => acc + curr);
-    const updatedAllowancesFromToday = daysFromToday.map(item => {
-        const allowance = (sumOfPreviousAllowances+sumOfFutureAllowances)/daysFromToday.length
-        return {...item, allowance }}); 
-        const newBreakdown = [...daysUntilToday, ...updatedAllowancesFromToday];  
-        // console.log('today', daysUntilToday);
-        // console.log('update', updatedAllowancesFromToday);
-        // console.log('new', newUpdate); 
-return newBreakdown;
-}
+      })
+      .reduce((acc, curr) => acc + curr);
+    const sumOfFutureAllowances = daysFromToday
+      .map((item) => {
+        return item.allowance || 0;
+      })
+      .reduce((acc, curr) => acc + curr);
+    const updatedAllowancesFromToday = daysFromToday.map((item) => {
+      const allowance =
+        (sumOfPreviousAllowances + sumOfFutureAllowances) /
+        daysFromToday.length;
+      return { ...item, allowance };
+    });
+    const newBreakdown = [...daysUntilToday, ...updatedAllowancesFromToday];
+    return newBreakdown;
+  };
 
   const launchModal = async (index) => {
     let day = allowanceDays[index];
-    let result = await showModal({page: EditDay,props: {day: day, index:index}});
-    monthlyBreakdown.edit(index,result)
+    let result = await showModal({
+      page: EditDay,
+      props: { day: day, index: index },
+    });
+    monthlyBreakdown.edit(index, result);
     const newBreakdown = updateAllowancesFromTodays(allowances);
-    monthlyBreakdown.set(newBreakdown)
-    localStorage.setItem('breakdown', JSON.stringify(newBreakdown))
-    console.log(localStorage.getItem('breakdown'));
-  }
-  
+    monthlyBreakdown.set(newBreakdown);
+    localStorage.setItem("breakdown", JSON.stringify(newBreakdown));
+  };
+
   const onItemTap = (event) => {
-    if ((event.index + 1) >= dayInMonth) {
+    if (event.index + 1 >= dayInMonth) {
       launchModal(event.index);
     }
   };
 </script>
 
 <style>
-
 </style>
 
 <page>
@@ -73,20 +83,24 @@ return newBreakdown;
   <dockLayout backgroundColor="yellow">
     <scrollView dock="top" backgroundColor="goldenrod">
       <listView
-      height="500"
-      on:itemTap={onItemTap}
-      items={allowances}
-      backgroundColor="blue">
-      <Template let:item >
-        <label width="300" height='100' backgroundColor="limegreen" text={parseFloat(item.allowance).toFixed(2)}>
-          <!-- {#if item.deductions.length} -->
+        height="500"
+        on:itemTap={onItemTap}
+        items={allowances}
+        backgroundColor="blue">
+        <Template let:item>
+          <label
+            width="300"
+            height="100"
+            backgroundColor="limegreen"
+            text={parseFloat(item.allowance).toFixed(2)}>
+            <!-- {#if item.deductions.length} -->
             {#each item.deductions as deduction }
             <span text={`- ${deduction}`}/>
             {/each}
-          <!-- {#if item.deductions.length}<span>additions</span>{/if} -->
-        </label>
-      </Template>
-    </listView>
+            <!-- {#if item.deductions.length}<span>additions</span>{/if} -->
+          </label>
+        </Template>
+      </listView>
     </scrollView>
     <button dock="bottom" text="Back" on:tap={goBack} />
   </dockLayout>
